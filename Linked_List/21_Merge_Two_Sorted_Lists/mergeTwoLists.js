1380f6af17b1fd0e628d4f7e3436e86766010252
Wilson Chen <wilsonchen901@gmail.com>
2023-04-04 16:33:54 -0400

    complete leetcode 21

--- Linked_List/21_Merge_Two_Sorted_Lists/mergeTwoLists.js
+++ Linked_List/21_Merge_Two_Sorted_Lists/mergeTwoLists.js
@@ -14,3 +14,24 @@
 
 // Input: list1 = [], list2 = [0]
 // Output: [0]
+
+
+function mergeTwoLists (list1, list2) {
+    let tempNode = new ListNode(0, null);
+    let currentNode = tempNode;
+
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            currentNode.next = list1;
+            list1 = list1.next;
+        } else {
+            currentNode.next = list2;
+            list2 = list2.next
+        };
+        currentNode = currentNode.next;
+    };
+
+    currentNode.next = list1 || list2;
+    return tempNode.next;
+}
+
