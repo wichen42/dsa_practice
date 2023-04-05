ca790288b8df649d16fa775139d60a021748b25d
Wilson Chen <wilsonchen901@gmail.com>
2023-04-05 16:23:14 -0400

    complete leetcode 226

--- Binary_Tree/226_Invert_Binary_Tree/invertTree.js
+++ Binary_Tree/226_Invert_Binary_Tree/invertTree.js
@@ -12,5 +12,14 @@
  
 
 function invertTree(root) {
+    if (root === null) return null;
 
+    let temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+
+    invertTree(root.left);
+    invertTree(root.right);
+
+    return root;
 };
